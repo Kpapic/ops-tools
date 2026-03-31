70512e7f9988ff64cc9f093089938f8e2dc77d65
Krešimir Papić <kresimir.papic@hotmail.com>
2026-03-31 11:26:47 +0200

    Add combined tool and the cleanup pending chnages

--- errors_last_minutes.sh
+++ errors_last_minutes.sh
@@ -1,120 +1,106 @@
 #!/bin/bash
 #
-# Showing last N lines in the last M minutes
-# Default value for the log file
-# It can be chnages with -f or --file arguments
+#
 LOGFILE="/opt/netzwert/log/CentralError.log"
-
 N=10
 M=60
 
-# Usage function - showing how the help option is used
-#
-usage () {
 
-echo "Use: $0 [-f log_file] [-n number] [-m minutes] [-h]"
-    echo "  -f FILE     path to log file (default: $LOGFILE)"
-    echo "  -n N        last N lines (default: $N)"
-    echo "  -m MIN      time window in minutes back (default: $M)"
-    echo "  -h          show help"
+usage() {
+    echo "Use: $0 [-f logfile] [-n number] [-m minutes] [-h]"
+    echo " -f FILE     Path to log file (default: $LOGFILE)"
+    echo " -n N        Last N lines (default: $N)"
+    echo " -m MIN      Window size in minutes (default: $M)"
+    echo " -h          Show help"
     echo
     echo "Long options:"
-    echo "  --file PATH     same as -f"
-    echo "  --number N      same as -n"
-    echo "  --minutes MIN   same as -m"
-    echo "  --help          same as -h"
+    echo " --file PATH"
+    echo " --number N"
+    echo " --minutes MIN"
+    echo " --help"
 }
 
-# Parsing long options
-#
-for arg in "$@"; do
-	case "$arg" in
-		--help)
-			usage
-			exit 0
-			;;
-		--file)
-			shift
-			LOGFILE="$1"
-			shift
-			;;
-		--number)
-			shift
-			N="$1"
-			shift
-			;;
-		--minutes)
-			shift
-			M="$1"
-			shift
-			;;
-		esac
-	done
 
-# Short Options
-#
+for arg in "$@"; do
+case "$arg" in
+	--help)
+		usage
+		exit 0
+		;;
+	--file)
+		shift
+		LOGFILE="$1"
+		shift
+		;;
+	--number)
+		shift
+		N="$1"
+		shift
+		;;
+	--minutes)
+		shift
+		M="$1"
+		shift
+		;;
+esac
+done
+
 while getopts ":f:n:m:h" opt; do
 	case "$opt" in
 		f) LOGFILE="$OPTARG" ;;
 		n) N="$OPTARG" ;;
 		m) M="$OPTARG" ;;
 		h) usage; exit 0 ;;
-
-		\?)
-			echo "Unknown option: -$OPTARG"
-			usage
-			exit 1 
-			;;
-		:)
-			echo "Option: -$OPTARG requires an argument"
-			usage
-			exit 1
-			;;
-		esac
-	done
-
+		\?) echo "Unknown option: -$OPTARG"; usage; exit 1 ;;
+		:) echo "Option -$OPTARG requires an argument"; exit 1 ;;
+	esac
+done
 
 case "$N" in
 	''|*[!0-9]*|0)
-		echo "Error: -n / --number must be a positive interger: $N"
+		echo "Error -n / --number must be positive integer"
 		exit 1
 		;;
 esac
 
 case "$M" in
 	''|*[!0-9]*|0)
-		echo "Error: -m / --minutes must be a positive interger: $M"
-		exit 1
-		;;
+		echo "Error -m / --minutes must be positive integer"
 esac
 
-# Checking log file
-#
 if [ ! -f "$LOGFILE" ]; then
-	echo "Error: log file does not exists: $LOGFILE"
+	echo "Error: log file does not exist: $LOGFILE"
 	exit 1
 fi
 
-# Time frame
-#
-NOW_SYS=$(date "+%b %e %H:%m")
-PAST_SYS=&(date -d "$M minutes ago" "+%b %e %H:%m")
+NOW_SYS=$(date "+%b %e %H:%M")
+PAST_SYS=$(date -d "$M minutes ago" "+%b %e %H:%M")
 
-NOW_NW=$(date "+%Y%m%d:%H%m")
-PAST_NW=$(date -d "$M minutes ago" "+%Y%m%d:%H:%m")
+NOW_NW=$(date "+%Y%m%d:%H:%M")
+PAST_NW=$(date -d "$M minutes ago" "+%Y%m%d:%H:%M")
 
-
-echo "Logfile: $LOGFILE"
-echo "Window: last $M minutes"
-echo "Anchors: $PAST_SYS .. $NOW_SYS"
-echo "Last $N ERROR lines in window"
-echo "================================="
-
-results="$(grep -i "error" "$LOGFILE" | grep -E "$NOW_SYS|$PAST_SYS|$NOW_NW|$PAST_NW")"
+results="$(
+awk '
+BEGIN {
+IGNORECASE = 1
+FS = "[[:space:]]+"
+}
+/error/ {
+print $1, $2, $4
+print $5
+rest = ""
+for (i=6; i<=NF; i++) {
+	rest = rest (i == 6 ? "" : " ") $i
+}
+print rest
+print "---------------------"
+}
+' "$LOGFILE"
+)"
 
 if [ -z "$results" ]; then
-	echo "No error lines found in the last $M minutes"
+	echo "No Error lines found in last $M minutes"
 else
-	printf "%s\n" "$results" | tail -n "$N"
-fi
+	echo "$results" | tail -n "$N"
+	fi
 
